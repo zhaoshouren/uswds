3fab2a6d1917cd6de019dd3db90e22a45bb92a7d
Marco Segreto <marco.segreto@gsa.gov>
2015-07-02 16:57:24 -0700

    A few changes, just trying out

--- assets/js/components.js
+++ assets/js/components.js
@@ -2,63 +2,85 @@
 
 /* globals $: false */
 
-/**
- * Accordion
- *
- * An accordion component.
- *
- * @param {jQuery} $el A jQuery html element to turn into an accordion.
- */
-function Accordion($el) {
-  var self = this;
-  this.$root = $el;
-  this.$root.on('click', 'button', function(ev) {
-    ev.preventDefault();
-    self.hideAll();
-    self.show($(this));
-  });
-}
+var usa = usa || {};
 
-Accordion.prototype.$ = function(selector) {
-  return this.$root.find(selector);
-}
+(function() {
+  /**
+   * Accordion
+   *
+   * An accordion component.
+   *
+   * @param {jQuery} $el A jQuery html element to turn into an accordion.
+   */
+  function Accordion($el) {
+    var self = this;
+    this.$root = $el;
+    this.$root.on('click', 'button', function(ev) {
+      ev.preventDefault();
+      self.hideAll();
+      self.show($(this));
+      self.$root.trigger(Accordion.EV_TAB_OPEN, $(this));
+    });
+  }
 
-Accordion.prototype.hide = function($button) {
-  var selector = $button.attr('aria-controls'),
-      $content = this.$('#' + selector);
+  Accordion.EV_TAB_OPEN = 'accordion-event-tab_open';
 
-  $button.attr('aria-expanded', false);
-  $content.attr('aria-hidden', true);
-};
+  Accordion.prototype.$ = function(selector) {
+    return this.$root.find(selector);
+  };
 
-Accordion.prototype.show = function($button) {
-  var selector = $button.attr('aria-controls'),
-      $content = this.$('#' + selector);
+  Accordion.prototype.on = function(event, cb) {
+    return this.$root.on(event, cb);
+  };
 
-  $button.attr('aria-expanded', true);
-  $content.attr('aria-hidden', false);
-};
+  Accordion.prototype.destroy = function() {
+    this.$root.off();
+    this.$root.remove();
+  };
 
-Accordion.prototype.hideAll = function() {
-  var self = this;
-  this.$('button').each(function() {
-    self.hide($(this));
-  });
-};
+  Accordion.prototype.hide = function($button) {
+    var selector = $button.attr('aria-controls'),
+        $content = this.$('#' + selector);
 
-/**
- * accordion
- *
- * Initialize a new Accordion component.
- *
- * @param {jQuery} $el A jQuery html element to turn into an accordion.
- */
-function accordion($el) {
-  return new Accordion($el);
-}
+    $button.attr('aria-expanded', false);
+    $content.attr('aria-hidden', true);
+  };
+
+  Accordion.prototype.show = function($button) {
+    var selector = $button.attr('aria-controls'),
+        $content = this.$('#' + selector);
+
+    $button.attr('aria-expanded', true);
+    $content.attr('aria-hidden', false);
+  };
+
+  Accordion.prototype.hideAll = function() {
+    var self = this;
+    this.$('button').each(function() {
+      self.hide($(this));
+    });
+  };
+  usa.Accordion = Accordion;
+
+  /**
+   * accordion
+   *
+   * Initialize a new Accordion component.
+   *
+   * @param {jQuery} $el A jQuery html element to turn into an accordion.
+   */
+  function accordion($el) {
+    return new Accordion($el);
+  }
+  usa.accordion = accordion;
+
+}).call(this);
 
 $(function() {
   $('.usa-accordion').each(function() {
-    accordion($(this));
+    var a = usa.accordion($(this));
+    a.on(usa.Accordion.EV_TAB_OPEN, function(ev, $control) {
+      console.log('event tab open', ev, $control);
+    });
   });
 });
