f8c4445f5110aa8751e4e4363c53408725bbbd94
msecret <msegreto@miceover.com>
2015-07-01 19:59:01 -0700

    Trying stuff out

--- _components/accordions.md
+++ _components/accordions.md
@@ -7,18 +7,18 @@ title: Accordions
 <h3>Bordered Accordion</h3>
 
 <div class="code-sample">
-  <ul id="accordion-menu">
+  <ul class="usa-accordion">
     <li>
-      <button class="button-unstyled"
+      <button class="usa-button-unstyled"
           aria-expanded="true" aria-controls="collapsible-0">
         Apple Pie h4
       </button>
-      <div id="collapsible-0" aria-hidden="false" class="content">
+      <div id="collapsible-0" aria-hidden="false" class="usa-accordion-content">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </div>
     </li>
     <li>
-      <button class="button-unstyled"
+      <button class="usa-button-unstyled"
           aria-expanded="false" aria-controls="collapsible-1">
         Stawberry Pie h4
       </button>
@@ -27,7 +27,7 @@ title: Accordions
       </div>
     </li>
     <li>
-      <button class="button-unstyled"
+      <button class="usa-button-unstyled"
           aria-expanded="false" aria-controls="collapsible-2">
         Peace Pie h4
       </button>
@@ -36,7 +36,7 @@ title: Accordions
       </div>
     </li>
     <li>
-      <button class="button-unstyled"
+      <button class="usa-button-unstyled"
           aria-expanded="false" aria-controls="collapsible-3">
         Key Lime h4
       </button>
@@ -45,7 +45,7 @@ title: Accordions
       </div>
     </li>
     <li>
-      <button class="button-unstyled"
+      <button class="usa-button-unstyled"
           aria-expanded="false" aria-controls="collapsible-3">
         Blackberry h4
       </button>
--- assets/js/components.js
+++ assets/js/components.js
@@ -1,28 +1,80 @@
-function hide($el) {
-  var selector = $el.attr('aria-controls'),
-      $content = $('#' + selector);
 
-  $el.attr('aria-expanded', false);
+'use strict';
+
+
+var DEFAULTS = {
+  controlSelector: 'button',
+  contentSelector: '.usa-accordion-content'
+};
+
+/**
+ * Accordion
+ *
+ * @param $el {jQuery} Element to apply accordion to.
+ * @param opts {Object} Options for the settings.
+ */
+function accordion($el, opts) {
+  if (this instanceof accordion) {
+    this.$root = $el;
+    var $first = this.$root.find(this.controlSelector).first(),
+        self = this;
+
+    this.settings = $.extend({}, DEFAULTS, opts);
+    this.controlSelector = this.settings.controlSelector;
+    this.contentSelector = this.settings.contentSelector;
+    this.hideAll();
+    this.show($first);
+
+    this.$root.find(this.controlSelector).on('click', function(ev) {
+      ev.preventDefault();
+      self.hideAll();
+      self.show($(this));
+    });
+  } else {
+    return new accordion($el, opts);
+  }
+}
+
+accordion.prototype.hide = function($control) {
+  var selector = $control.attr('aria-controls'),
+      $content = this.$root.find('#' + selector);
+
+  $control.attr('aria-expanded', false);
   $content.attr('aria-hidden', true);
 }
 
-function show($el) {
-  var selector = $el.attr('aria-controls'),
+accordion.prototype.show = function($control) {
+  var selector = $control.attr('aria-controls'),
       $content = $('#' + selector);
 
-  $el.attr('aria-expanded', true);
+  $control.attr('aria-expanded', true);
   $content.attr('aria-hidden', false);
 }
 
-function hideAll($container) {
-  $container.find('button').each(function() {
-    hide($(this));
+accordion.prototype.hideAll = function() {
+  var self = this;
+  this.$root.find(this.controlSelector).each(function() {
+    self.hide($(this));
   });
 }
 
 $(function() {
-  $('#accordion-menu').on('click', 'button', function(ev) {
-    hideAll($('#accordion-menu'));
-    show($(this));
+  $('.usa-accordion').each(function() {
+    accordion($(this));
   });
 });
+
+(function() {
+  if (typeof define === 'function' && define.amd) {
+    define(function () {
+      return accordion;
+    });
+  }
+  else if (typeof module !== 'undefined' && module.exports) {
+    module.exports = accordion;
+  }
+  else {
+    this.usa = this.usa || {};
+    this.usa.accordion = accordion;
+  }
+}).call(this);
